33c02eebb106c834acbca19699dcd669bb9d32d5
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-09-23 14:43:40 +0900

    Update test

--- pkg/evaluator/evaluator_test.go
+++ pkg/evaluator/evaluator_test.go
@@ -975,16 +975,13 @@ func Example_shor() {
 	qasm := `
 OPENQASM 3.0;
 
-const N = 3 * 5;
-const a = 7;
-
 qubit[3] r0;
 qubit[4] r1;
 reset r0, r1;
 
 X r1[-1];
 H r0;
-CMODEXP2(a, N) r0, r1;
+CMODEXP2(7, 15) r0, r1;
 IQFT r0;
 `
 
