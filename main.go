da13455696763060cf547ede646e4c2292d7f03e
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-05-31 19:39:29 +0900

    Update main.go

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func New() *cli.App {
 	app.HelpName = "qasm"
 	app.Usage = "Run Quantum Computation Simulator with OpenQASM 3.0"
 	app.Action = cmd.Action
-	app.Version = "0.0.2"
+	app.Version = "0.0.7"
 	app.Flags = []cli.Flag{
 		&cli.StringFlag{
 			Name:    "file",
