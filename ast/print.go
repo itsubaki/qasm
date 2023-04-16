0a8f37203128b6ebddeff9087da34f3f7d295108
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-04-16 12:16:00 +0000

    Update some files

--- ast/print.go
+++ ast/print.go
@@ -52,6 +52,70 @@ func (p *printer) printf(format string, a ...interface{}) error {
 	return nil
 }
 
+func (p *printer) slice(x reflect.Value) {
+	p.printf("%s (len = %d) {", x.Type(), x.Len())
+	if x.Len() > 0 {
+		p.indent++
+		p.printf("\n")
+		for i, n := 0, x.Len(); i < n; i++ {
+			p.printf("%d: ", i)
+			p.print(x.Index(i))
+			p.printf("\n")
+		}
+		p.indent--
+	}
+	p.printf("}")
+}
+
+func (p *printer) field(i int, t reflect.Type, v reflect.Value) bool {
+	name := t.Field(i).Name
+	p.printf("%s: ", name)
+	if name == "Kind" {
+		lit := lexer.Tokens[v.Int()]
+		p.print(reflect.ValueOf(lit))
+		p.printf("\n")
+		return true
+	}
+
+	p.print(v)
+	return false
+}
+
+func (p *printer) strct(x reflect.Value) {
+	t := x.Type()
+	p.printf("%s {", t)
+	p.indent++
+
+	first := true
+	for i, n := 0, t.NumField(); i < n; i++ {
+		value := x.Field(i)
+		if isNil(value) {
+			continue
+		}
+
+		switch v := value.Interface().(type) {
+		case string:
+			if len(v) == 0 {
+				continue
+			}
+		}
+
+		if first {
+			p.printf("\n")
+			first = false
+		}
+
+		if p.field(i, t, value) {
+			continue
+		}
+
+		p.printf("\n")
+	}
+
+	p.indent--
+	p.printf("}")
+}
+
 func (p *printer) print(x reflect.Value) {
 	if isNil(x) {
 		p.printf("nil")
@@ -59,65 +123,18 @@ func (p *printer) print(x reflect.Value) {
 	}
 
 	switch x.Kind() {
-	case reflect.Interface:
-		p.print(x.Elem())
-
 	case reflect.Ptr:
 		p.printf("*")
 		p.print(x.Elem())
 
-	case reflect.Struct:
-		t := x.Type()
-		p.printf("%s {", t)
-		p.indent++
-
-		first := true
-		for i, n := 0, t.NumField(); i < n; i++ {
-			value := x.Field(i)
-			if isNil(value) {
-				continue
-			}
-
-			switch v := value.Interface().(type) {
-			case string:
-				if len(v) == 0 {
-					continue
-				}
-			}
-
-			if first {
-				p.printf("\n")
-				first = false
-			}
-
-			name := t.Field(i).Name
-			p.printf("%s: ", name)
-			if name == "Kind" {
-				lit := lexer.Tokens[value.Int()]
-				p.print(reflect.ValueOf(lit))
-				p.printf("\n")
-				continue
-			}
-
-			p.print(value)
-			p.printf("\n")
-		}
-		p.indent--
-		p.printf("}")
+	case reflect.Interface:
+		p.print(x.Elem())
 
 	case reflect.Slice:
-		p.printf("%s (len = %d) {", x.Type(), x.Len())
-		if x.Len() > 0 {
-			p.indent++
-			p.printf("\n")
-			for i, n := 0, x.Len(); i < n; i++ {
-				p.printf("%d: ", i)
-				p.print(x.Index(i))
-				p.printf("\n")
-			}
-			p.indent--
-		}
-		p.printf("}")
+		p.slice(x)
+
+	case reflect.Struct:
+		p.strct(x)
 
 	default:
 		p.printf("%v", x)
