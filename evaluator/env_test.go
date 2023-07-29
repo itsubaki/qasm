dcf03b8b5e988db4cabf68f17c3772e1fa9ea1ae
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-07-29 05:54:38 +0000

    Add test

--- evaluator/env_test.go
+++ evaluator/env_test.go
@@ -8,7 +8,35 @@ import (
 	"github.com/itsubaki/qasm/evaluator"
 )
 
-func ExampleQubit_All() {
+func ExampleBit() {
+	b := &evaluator.Bit{
+		Name:  make([]string, 0),
+		Value: make(map[string][]int64),
+	}
+	b.Add(&ast.IdentExpr{Name: "a"}, []int64{1, 2, 3})
+	b.Add(&ast.IdentExpr{Name: "b"}, []int64{4, 5, 6})
+	fmt.Println(b)
+
+	fmt.Println(b.Get(&ast.IndexExpr{Name: "c"}))
+	fmt.Println(b.Get(&ast.IndexExpr{Name: "c", Value: "1"}))
+	fmt.Println(b.Get(&ast.IndexExpr{Name: "a", Value: "0"}))
+	fmt.Println(b.Get(&ast.IndexExpr{Name: "b", Value: "-1"}))
+	fmt.Println(b.Get(&ast.IndexExpr{Name: "b", Value: "10"}))
+	fmt.Println(b.Get(&ast.IndexExpr{Name: "b", Value: "-10"}))
+	fmt.Println(b.Get(&ast.BadExpr{}))
+
+	// Output:
+	// [a b], map[a:[1 2 3] b:[4 5 6]]
+	// [] false
+	// [] false
+	// [1] true
+	// [6] true
+	// [] false
+	// [] false
+	// [] false
+}
+
+func ExampleQubit() {
 	qb := &evaluator.Qubit{
 		Name:  make([]string, 0),
 		Value: make(map[string][]q.Qubit),
@@ -18,8 +46,26 @@ func ExampleQubit_All() {
 	qb.Add(&ast.GenDecl{Name: "q1"}, []q.Qubit{4, 5, 6})
 	qb.Add(&ast.GenDecl{Name: "q2"}, []q.Qubit{7, 8, 9})
 
+	fmt.Println(qb)
 	fmt.Println(qb.All())
+	fmt.Println(qb.Get(&ast.IndexExpr{Name: "c"}))
+	fmt.Println(qb.Get(&ast.IndexExpr{Name: "q0", Value: "10"}))
+	fmt.Println(qb.Get(&ast.IndexExpr{Name: "q0", Value: "-10"}))
+	fmt.Println(qb.Get(&ast.BadExpr{}))
 
 	// Output:
+	// [q0 q1 q2], map[q0:[1 2 3] q1:[4 5 6] q2:[7 8 9]]
 	// [1 2 3 4 5 6 7 8 9] <nil>
+	// [] false
+	// [] false
+	// [] false
+	// [] false
+}
+
+func ExampleEnviron() {
+	env := evaluator.NewEnviron()
+	fmt.Println(env)
+
+	// Output:
+	// gatedef: map[], const: map[], bit: [], map[], qubit: [], map[], modifier: [], decl: []
 }
