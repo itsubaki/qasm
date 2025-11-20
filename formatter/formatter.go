9b8435ce575209048afc9e8dcb49033586d6a26f
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-11-20 13:15:57 +0000

    Update formatter

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -2,16 +2,19 @@ package formatter
 
 import (
 	"github.com/antlr4-go/antlr/v4"
+	"github.com/itsubaki/qasm/gen/parser"
 )
 
 type Formatter struct {
+	*parser.Baseqasm3ParserListener
 	rewriter *antlr.TokenStreamRewriter
 	indent   int
 }
 
 func New(tokens *antlr.CommonTokenStream) *Formatter {
 	return &Formatter{
-		rewriter: antlr.NewTokenStreamRewriter(tokens),
-		indent:   0,
+		Baseqasm3ParserListener: &parser.Baseqasm3ParserListener{},
+		rewriter:                antlr.NewTokenStreamRewriter(tokens),
+		indent:                  0,
 	}
 }
