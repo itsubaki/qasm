f40de0dc4d4c50edf21aa0f73d6d76367b2f6f52
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-11-21 19:18:42 +0900

    Update formatter

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -7,14 +7,39 @@ import (
 
 type Formatter struct {
 	*parser.Baseqasm3ParserListener
-	rewriter *antlr.TokenStreamRewriter
-	indent   int
+	rewriter    *antlr.TokenStreamRewriter
+	programName string
 }
 
 func New(tokens *antlr.CommonTokenStream) *Formatter {
 	return &Formatter{
 		Baseqasm3ParserListener: &parser.Baseqasm3ParserListener{},
 		rewriter:                antlr.NewTokenStreamRewriter(tokens),
-		indent:                  0,
+		programName:             "default",
 	}
+}
+
+func (f *Formatter) ExitVersion(ctx *parser.VersionContext) {
+	f.rewriter.InsertBefore(f.programName, ctx.GetStart().GetTokenIndex()+1, " ")
+	f.rewriter.InsertAfter(f.programName, ctx.GetStop().GetTokenIndex(), "\n")
+}
+
+func (f *Formatter) ExitStatement(ctx *parser.StatementContext) {
+	f.rewriter.InsertAfter(f.programName, ctx.GetStop().GetTokenIndex(), "\n")
+}
+
+func (f *Formatter) ExitIncludeStatement(ctx *parser.IncludeStatementContext) {
+	f.rewriter.InsertBefore(f.programName, ctx.GetStart().GetTokenIndex()+1, " ")
+}
+
+func (f *Formatter) ExitGateCallStatement(ctx *parser.GateCallStatementContext) {
+	f.rewriter.InsertBefore(f.programName, ctx.GetStart().GetTokenIndex()+1, " ")
+}
+
+func (f *Formatter) ExitQubitType(ctx *parser.QubitTypeContext) {
+	f.rewriter.InsertAfter(f.programName, ctx.GetStop().GetTokenIndex(), " ")
+}
+
+func (f *Formatter) Format() string {
+	return f.rewriter.GetTextDefault()
 }
--- formatter/formatter_test.go
+++ formatter/formatter_test.go
@@ -1,6 +1,8 @@
 package formatter_test
 
 import (
+	"fmt"
+
 	"github.com/antlr4-go/antlr/v4"
 	"github.com/itsubaki/qasm/formatter"
 	"github.com/itsubaki/qasm/gen/parser"
@@ -14,7 +16,22 @@ func ExampleNew() {
 	`
 
 	lexer := parser.Newqasm3Lexer(antlr.NewInputStream(text))
-	formatter.New(antlr.NewCommonTokenStream(lexer, antlr.TokenHiddenChannel))
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	p := parser.Newqasm3Parser(stream)
+
+	tree := p.Program()
+	f := formatter.New(stream)
+	antlr.ParseTreeWalkerDefault.Walk(f, tree)
+
+	result := f.Format()
+	fmt.Println(result)
 
 	// Output:
+	// OPENQASM 3.0;
+	// include "../testdata/stdgates.qasm";
+	// qubit q0;
+	// qubit[2] q1;
+	// qubit[2] q2;
+	// x q1;
+	// h q2;
 }
