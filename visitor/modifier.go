e41c9a1dec9059570737fd3199827a98aa41d5a7
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-04-25 13:55:13 +0900

    Update modifier.go

--- visitor/modifier.go
+++ visitor/modifier.go
@@ -43,23 +43,21 @@ func Pow2x2(u *matrix.Matrix, p float64, tol ...float64) *matrix.Matrix {
 
 	// phase**p
 	phaseP := cmplx.Pow(phase, complex(p, 0))
+	idp := matrix.Identity(2).Mul(phaseP)
 
 	// if sin(theta) is close to zero, su is close to I or -I.
 	if epsilon.IsZeroF64(sinTheta, tol...) {
-		id := matrix.Identity(2).Mul(phaseP)
 		if cosTheta > 0 {
-			return id
+			return idp
 		}
 
-		return id.Mul(cmplx.Exp(complex(0, p*math.Pi)))
+		return idp.Mul(cmplx.Exp(complex(0, p*math.Pi)))
 	}
 
-	id := matrix.Identity(2)
 	a := su.Sub(su.Dagger()).Mul(complex(0, -0.5/sinTheta))
 
 	// p*theta
 	cos := complex(math.Cos(p*theta), 0)
 	sin := complex(0, math.Sin(p*theta))
-
-	return id.Mul(cos).Add(a.Mul(sin)).Mul(phaseP)
+	return idp.Mul(cos).Add(a.Mul(sin))
 }
