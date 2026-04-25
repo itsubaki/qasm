fe6b80d8416ce454902aa5b6c51bf09dbc06efb6
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-04-25 11:29:04 +0900

    Update Pow2x2

--- visitor/modifier.go
+++ visitor/modifier.go
@@ -1,8 +1,11 @@
 package visitor
 
 import (
+	"math"
+	"math/cmplx"
 	"slices"
 
+	"github.com/itsubaki/q/math/epsilon"
 	"github.com/itsubaki/q/math/matrix"
 	"github.com/itsubaki/qasm/gen/parser"
 )
@@ -27,11 +30,41 @@ func ReversedModifier(ctx parser.IGateCallStatementContext) []parser.IGateModifi
 
 // Pow2x2 returns u**p for 2x2 matrix u and float p.
 // If p is negative, returns (u-dagger)**p.
-func Pow2x2(u *matrix.Matrix, p float64) *matrix.Matrix {
+func Pow2x2(u *matrix.Matrix, p float64, tol ...float64) *matrix.Matrix {
 	if p < 0 {
 		p, u = -p, u.Dagger()
 	}
 
-	// TODO: support float type
-	return matrix.ApplyN(u, int(p))
+	// SU
+	det := u.At(0, 0)*u.At(1, 1) - u.At(0, 1)*u.At(1, 0)
+	phase := cmplx.Sqrt(det)
+	su := u.Mul(1 / phase)
+
+	// theta
+	tr := real(su.Trace())
+	cosTheta := max(min(tr/2, 1), -1)
+	theta := math.Acos(cosTheta)
+	sinTheta := math.Sin(theta)
+
+	// phase**p
+	phaseP := cmplx.Pow(phase, complex(p, 0))
+
+	// If sin(theta) is zero, then u is either I or -I.
+	if epsilon.IsZeroF64(sinTheta, tol...) {
+		if cosTheta > 0 {
+			return su.Mul(phaseP)
+		}
+
+		id := matrix.Identity(2)
+		return id.Mul(complex(math.Cos(p*math.Pi), 0)).Mul(phaseP)
+	}
+
+	id := matrix.Identity(2)
+	a := su.Sub(su.Dagger()).Mul(complex(0, -0.5/sinTheta))
+
+	// p*theta
+	cos := complex(math.Cos(p*theta), 0)
+	sin := complex(0, math.Sin(p*theta))
+
+	return id.Mul(cos).Add(a.Mul(sin)).Mul(phaseP)
 }
