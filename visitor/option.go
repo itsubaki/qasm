8f3ccd6216d2097ecfed7a9ce881fd1ff7c28657
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-04-26 23:43:20 +0900

    Add godoc

--- visitor/option.go
+++ visitor/option.go
@@ -1,7 +1,9 @@
 package visitor
 
+// Option is a function that modifies the Visitor.
 type Option func(*Visitor)
 
+// WithMaxQubits sets the maximum number of qubits for the visitor.
 func WithMaxQubits(n int) Option {
 	return func(v *Visitor) {
 		v.maxQubits = n
